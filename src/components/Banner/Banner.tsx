f084b52a2382e9a6597aac57855f4aa34490e686
Hem Bhadur <sushilkc2611@gmail.com>
2025-04-17 12:48:08 +0530

    fix banner issue

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,6 +1,4 @@
 // import Image from "next/image";
-import Paragraph from "../Paragraph/Paragraph";
-import Button from "../Button";
 import LazyLoadedVideo from "../LazyLoadedVideo";
 
 interface BannerProps {
@@ -12,31 +10,33 @@ interface BannerProps {
     href: string;
   }[];
 }
-const Banner: React.FC<BannerProps> = ({ title, desc, src, buttons }) => {
+const Banner: React.FC<BannerProps> = ({ title, src }) => {
   return (
     <section className="max-w-[1600px] mx-auto bg-primary text-white lg:mb-10">
-      <div className="grid lg:grid-cols-9 items-center gap-3 justify-center grid-cols-1">
-        <div className="lg:col-span-4 w-full max-md:px-5 max-md:pt-4">
-          <div className="flex flex-col gap-4 ml-auto max-w-lg w-full">
-            <h1 className="largeHeading font_go uppercase letter_spacing font-semibold max-sm:text-center">
-              {title}
-            </h1>
-            <Paragraph className={"mt-4 description1 text-white max-sm:text-center"} text={desc} />
-            <div className="flex items-center max-sm:justify-center gap-5 mt-4">
-              {buttons.map((button, i) => (
-                <Button
-                  key={i}
-                  label={button.label}
-                  href={button.href}
-                  className={`px-6 text-center ${i === 0 ? "bg-secondary" : "bg-white !text-secondary"} `}
-                />
-              ))}
+      <div className="">
+        <div className="relative w-full lg:aspect-[4/1.6] aspect-[4/3]">
+          <LazyLoadedVideo src={src} poster={src} controls={false} />
+
+          <div className="absolute inset-0 flex items-center justify-center w-full h-full">
+            <div className="flex flex-col gap-4 max-w-lg w-full">
+              <h1 className="largeHeading font_go uppercase letter_spacing font-semibold max-sm:text-center">
+                {title}
+              </h1>
+              {/* <Paragraph
+                className={"mt-4 description1 text-white max-sm:text-center"}
+                text={desc}
+              /> */}
+              {/* <div className="flex items-center max-sm:justify-center gap-5 mt-4">
+                {buttons.map((button, i) => (
+                  <Button
+                    key={i}
+                    label={button.label}
+                    href={button.href}
+                    className={`px-6 text-center ${i === 0 ? "bg-secondary" : "bg-white !text-secondary"} `}
+                  />
+                ))}
+              </div> */}
             </div>
-          </div>
-        </div>
-        <div className="lg:col-span-5">
-          <div className="relative w-full lg:aspect-video aspect-[4/3]">
-            <LazyLoadedVideo src={src} poster={src} controls={false} />
           </div>
         </div>
       </div>
